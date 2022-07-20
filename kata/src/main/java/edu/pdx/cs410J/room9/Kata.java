2e855a5f2939fe802560debb7ed6a268a359c1ca
benttruong <108073148+benttruong@users.noreply.github.com>
2022-07-20 09:03:59 -0700

    some work that we did not push the other day

--- kata/src/main/java/edu/pdx/cs410J/room9/Kata.java
+++ kata/src/main/java/edu/pdx/cs410J/room9/Kata.java
@@ -8,7 +8,38 @@ package edu.pdx.cs410J.room9;
  */
 public class Kata {
 
+  private  int number;
+  public Kata(int i) {
+    this.number = i;
+  }
+
+
   public static void main(String[] args) {
     System.err.println("Missing command line arguments");
+  }
+
+
+  public String getString() {
+    String output = "";
+    String test = String.valueOf(number);
+    int x = number % 3;
+
+
+    switch(x) {
+      
+      case (0): {
+        output += "Foo";
+        if (test.contains("3"))
+          output +="Foo";
+      }
+    }
+
+    int numOf3 = 0;
+    for (char c:s){
+      if(c == '3')
+        ++numOf3;
+    }
+
+    return output;
   }
 }
--- kata/src/test/java/edu/pdx/cs410J/room9/KataTest.java
+++ kata/src/test/java/edu/pdx/cs410J/room9/KataTest.java
@@ -1,13 +1,18 @@
 package edu.pdx.cs410J.room9;
 
-import org.junit.jupiter.api.Test;
+import org.testng.annotations.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.StringContains.containsString;
 
 public class KataTest
 {
 
   @Test
-  void canInstantiateKataClass() {
-    new Kata();
+  void tryInputingNumber3(){
+    Kata instance = new Kata(3);
+    assertThat(instance.getString(), containsString("FooFoo"));
   }
+
+
 
 }
